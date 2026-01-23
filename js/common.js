817414eea493d91cf0faad9caddab2716decc5bc
hgonnnnn <hgonnnnn5@gmail.com>
2026-01-23 15:33:21 +0900

    20260123_15:33

---
+++ js/common.js
@@ -0,0 +1,19 @@
+$(function () {
+    $('#header').load('/common/header.html');
+    $('#footer').load('/common/footer.html');
+});
+/* 깃허브 업로드 시 common 앞부분에 폴더 경로 추가 */
+
+/* 스크롤 부드럽게 */
+const lenis = new Lenis({
+    duration: 1.2,   // 스크롤 속도
+    smooth: true,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+})
+
+function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
